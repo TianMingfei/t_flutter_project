39f2752ae3a0e411d6a6a67407b26ff4e4057afa
Tian_Mingfei <tianmingfei@taoshouyou.com>
2021-05-20 18:29:17 +0800

    async await

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 
 
 import 'package:flutter/material.dart';
+import "dart:io";
 
 void main() {
   // runApp(MyApp());
@@ -23,13 +24,40 @@ class MyApp extends StatelessWidget {
 }
 
 class HYHomePage extends StatelessWidget {
+
+  // Future<String> getNetworkData() {
+  //   return Future<String>(() {
+  //     sleep(Duration(seconds: 3));
+  //     // 不再返回结果，而是出现异常
+  //     return "network data";
+  //     // throw Exception("网络请求出现错误");
+  //   });
+  // }
+
+  Future getNetworkData() async {
+    var result = await Future.delayed(Duration(seconds: 1), () {
+      return "pppooo";
+    });
+    return result + "888999";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("GridView测试"),
       ),
-      body: Container()
+      body: Container(
+        child: TextButton(child: Text("FutureButton"), onPressed: () {
+          getNetworkData().then((value)  {
+            print(value);
+            return value + "111";
+          }).then((value) => value + "222")
+          .then((value) {
+            print(value);
+          });
+        },),
+      )
     );
   }
 }
